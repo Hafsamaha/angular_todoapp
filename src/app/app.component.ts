596638a350ed2832d8c4d8c8a4a3613f148b23ff
Sidi Lotfi El Alami <sidi.lotfi@docc.techstarter.de>
2024-01-19 09:26:24 +0100

    angular_hausaufgabe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { NgFor, NgIf } from "@angular/common";
+import { Component } from "@angular/core";
+import { Item } from "./item";
+import { ItemComponent } from "./item/item.component";
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    NgFor,ItemComponent, NgIf
+  ]
 })
 export class AppComponent {
-  title = 'todoapp';
+  title = "todo";
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+   
+  
 }
